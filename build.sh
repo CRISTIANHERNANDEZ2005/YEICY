1cc821204fe75374e8a2f3578f00e5bbf310dc10
Cristian Hernandez <cristianhernandez301205@gmail.com>
2025-07-05 12:58:24 -0500

    pro

--- build.sh
+++ build.sh
@@ -2,11 +2,11 @@
 
 # Actualizar el sistema e instalar dependencias necesarias
 apt-get update
-apt-get install -y python3-dev build-essential
+apt-get install -y python3-dev build-essential libjpeg-dev zlib1g-dev libpng-dev
 
 # Instalar las dependencias de Python
-pip install --upgrade pip
-pip install -r requirements.txt
+pip install --upgrade pip setuptools wheel
+pip install -r requirements.txt --no-cache-dir
 
 # Ejecutar las migraciones de Django y recolectar archivos estáticos
 python manage.py migrate --noinput
